2ff4de62de78c8e4111951f6989190da5eefb978
Thangnv <katothang@gmail.com>
2022-03-27 03:18:54 +0700

    update code

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,14 +1,1 @@
-console.log("start firebase");
-const firebaseConfig = {
-  apiKey: "AIzaSyB7DAMYuT_ynhCeR4eDQuVs7EbWKaOKnYI",
-  authDomain: "hocthi-7a94b.firebaseapp.com",
-  projectId: "hocthi-7a94b",
-  storageBucket: "hocthi-7a94b.appspot.com",
-  messagingSenderId: "455042665776",
-  appId: "1:455042665776:web:1bcade187a0937e9b70cf5",
-  measurementId: "G-JHHHE9TSDK"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-var db = firebase.firestore();
-
+const _0x1ab1b0=_0x4e27;(function(_0x2bd9ce,_0x2dba36){const _0x5bbc53=_0x4e27,_0x2ec6af=_0x2bd9ce();while(!![]){try{const _0x47e743=parseInt(_0x5bbc53(0xe0))/0x1+-parseInt(_0x5bbc53(0xdb))/0x2*(parseInt(_0x5bbc53(0xdf))/0x3)+-parseInt(_0x5bbc53(0xe3))/0x4*(-parseInt(_0x5bbc53(0xe4))/0x5)+-parseInt(_0x5bbc53(0xd8))/0x6*(-parseInt(_0x5bbc53(0xda))/0x7)+parseInt(_0x5bbc53(0xe2))/0x8+parseInt(_0x5bbc53(0xdd))/0x9*(-parseInt(_0x5bbc53(0xe1))/0xa)+-parseInt(_0x5bbc53(0xd7))/0xb;if(_0x47e743===_0x2dba36)break;else _0x2ec6af['push'](_0x2ec6af['shift']());}catch(_0x3c6416){_0x2ec6af['push'](_0x2ec6af['shift']());}}}(_0x2c5a,0x98834),console['log'](_0x1ab1b0(0xe7)));const firebaseConfig={'apiKey':_0x1ab1b0(0xe6),'authDomain':'hocthi-7a94b.firebaseapp.com','projectId':_0x1ab1b0(0xe5),'storageBucket':_0x1ab1b0(0xdc),'messagingSenderId':_0x1ab1b0(0xd9),'appId':'1:455042665776:web:1bcade187a0937e9b70cf5','measurementId':_0x1ab1b0(0xd6)};firebase[_0x1ab1b0(0xde)](firebaseConfig);function _0x2c5a(){const _0x91fb4a=['293458RwEHqW','163860ghPUSO','5739424oiWCea','1111688nXPFLU','5gBiicB','hocthi-7a94b','AIzaSyB7DAMYuT_ynhCeR4eDQuVs7EbWKaOKnYI','start\x20firebase','G-JHHHE9TSDK','7567736IkTKOb','18WvmszF','455042665776','1487556qiqZfO','5492xrxdfL','hocthi-7a94b.appspot.com','144QqklUL','initializeApp','384sUjNfZ'];_0x2c5a=function(){return _0x91fb4a;};return _0x2c5a();}function _0x4e27(_0x4af435,_0x50b536){const _0x2c5a8b=_0x2c5a();return _0x4e27=function(_0x4e278c,_0x19fe2c){_0x4e278c=_0x4e278c-0xd6;let _0x2cf5c2=_0x2c5a8b[_0x4e278c];return _0x2cf5c2;},_0x4e27(_0x4af435,_0x50b536);}var db=firebase['firestore']();
